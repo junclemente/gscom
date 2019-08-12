ff41985c730accb0c1a6bd0b4597933ba1b26ae9
Jun C <kjstudios858@gmail.com>
2019-08-12 00:27:59 -0700

    Add styles to carousel

--- src/components/Home/Carousel/index.jsx
+++ src/components/Home/Carousel/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Image1 from "./network_1.png";
-import Image2 from "./network_2.png";
+import Container from "react-bootstrap/Container";
+
+import Carousel1 from "./network_1.png";
+import Carousel2 from "./network_2.png";
+import styles from "./Carousel.module.scss";
 
 const HomeCarousel = () => (
-  <Carousel>
-    <Carousel.Item>
-      <img src={Image1} alt="Core Technologies" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src={Image2} alt="uwave" />
-    </Carousel.Item>
-  </Carousel>
+  <Container className={styles.root}>
+    <Carousel interval={6000}>
+      <Carousel.Item>
+        <img className={styles.image} src={Carousel1} alt="Core Technologies" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className={styles.image} src={Carousel2} alt="uwave" />
+      </Carousel.Item>
+    </Carousel>
+  </Container>
 );
 
 export default HomeCarousel;
