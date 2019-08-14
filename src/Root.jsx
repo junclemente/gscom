1300bf533a2a8d2826d517e7e7df91808438c36d
Jun C <kjstudios858@gmail.com>
2019-08-14 16:14:33 -0700

    Update path

--- src/Root.jsx
+++ src/Root.jsx
@@ -29,7 +29,8 @@ const Root = () => (
     <Navigation />
     <TopPadding />
     <Switch>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/gscom" component={Home} />
       <Route exact path={HOME_URL} component={Home} />
       <Route exact path={BUSINESS_URL} component={Business} />
       <Route exact path={PRODUCTS_URL} component={Products} />
