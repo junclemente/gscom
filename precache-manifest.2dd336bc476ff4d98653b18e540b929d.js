a8e45e82a72cf5f2af942b2e47254c0a1c5a3673
Jun C <kjstudios858@gmail.com>
2019-08-14 15:41:00 -0700

    Updates

---
+++ precache-manifest.2dd336bc476ff4d98653b18e540b929d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dcda4f08623c69c3c483d4cd92a2ed",
+    "url": "/gscom/index.html"
+  },
+  {
+    "revision": "2ea07cfebf8899d5f254",
+    "url": "/gscom/static/css/main.f7c79e68.chunk.css"
+  },
+  {
+    "revision": "aec7ec5124c69c783d58",
+    "url": "/gscom/static/js/2.d0a3ce1d.chunk.js"
+  },
+  {
+    "revision": "2ea07cfebf8899d5f254",
+    "url": "/gscom/static/js/main.3d02b666.chunk.js"
+  },
+  {
+    "revision": "a1b3913022dc0c2e9752",
+    "url": "/gscom/static/js/runtime~main.1070cc21.js"
+  },
+  {
+    "revision": "7ebe59d006ea3ea9031e8d1a49758ccd",
+    "url": "/gscom/static/media/AGC_Design.7ebe59d0.pdf"
+  },
+  {
+    "revision": "d6d8dcf51a08a90e77563764b55e0f1c",
+    "url": "/gscom/static/media/Broadband_wireless_4096-QAM_Modem.d6d8dcf5.pdf"
+  },
+  {
+    "revision": "fdaea599f2c3a155c819316d6116e0c0",
+    "url": "/gscom/static/media/Business_Development.fdaea599.pdf"
+  },
+  {
+    "revision": "1c35cfdffe771686ab9bb84039d29298",
+    "url": "/gscom/static/media/Business_Proposal.1c35cfdf.pdf"
+  },
+  {
+    "revision": "2dd956dfed4f02743f8ec4d08517765f",
+    "url": "/gscom/static/media/DFE_and_Carrier_Recovery.2dd956df.pdf"
+  },
+  {
+    "revision": "86cf4e6a04539e7bd3fb1db2917f205b",
+    "url": "/gscom/static/media/LDPC_System_Design.86cf4e6a.pdf"
+  },
+  {
+    "revision": "e768fb59667eda2bc34cbbb722c9aaaf",
+    "url": "/gscom/static/media/Mapper.e768fb59.pdf"
+  },
+  {
+    "revision": "dfbcaa19f2183f947b24d569d60eaee2",
+    "url": "/gscom/static/media/Project_Budget.dfbcaa19.pdf"
+  },
+  {
+    "revision": "10fd9309f0c584251a4f7bd7be08e808",
+    "url": "/gscom/static/media/Reed-Solomon_Codec.10fd9309.pdf"
+  },
+  {
+    "revision": "df5f7e296c6ea904d57ab72b0253a976",
+    "url": "/gscom/static/media/backhaul_1.df5f7e29.png"
+  },
+  {
+    "revision": "f59aa2ba6da28c34cd991e32a36a532e",
+    "url": "/gscom/static/media/backhaul_3.f59aa2ba.png"
+  },
+  {
+    "revision": "d2b691b9197b2fa6e24512d7d2937470",
+    "url": "/gscom/static/media/gscom_logo.d2b691b9.jpg"
+  },
+  {
+    "revision": "aa07bc571d78e7543212f3611015d998",
+    "url": "/gscom/static/media/gscom_map.aa07bc57.png"
+  },
+  {
+    "revision": "1ce338d98714b9d80cd82ab9541b0a99",
+    "url": "/gscom/static/media/network_1.1ce338d9.png"
+  },
+  {
+    "revision": "ab64dc676013461a97b7c2151982b57d",
+    "url": "/gscom/static/media/network_2.ab64dc67.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50e1802e4649b6c5ce5fe239c88a194d.js"
+  "/gscom/precache-manifest.2dd336bc476ff4d98653b18e540b929d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gscom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
